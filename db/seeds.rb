6b171631f9b4de2d054791d4cfba7ddcaf84f1ac
Alejandro Ventura Contreras <venturacontreras@icloud.com>
2018-04-22 00:54:20 -0500

    Create a default User.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Generate a secure random password for the default user
+# Go to '/users/sign_in' and click on 'Forgot your password?' and RESET your password
+# Set a new password
+randomPassword = Devise.friendly_token.first(8)
+mainAdminUser = User.new(
+	name: "Alejandro Ventura Contreras",
+	email: "lex.venturac@gmail.com",
+	password: randomPassword,
+	password_confirmation: randomPassword
+)
+mainAdminUser.skip_confirmation!
+mainAdminUser.save!
